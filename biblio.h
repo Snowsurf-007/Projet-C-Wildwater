4d62f5a48bb970c547247c63bb6556fcc80f81b2
Lucien LEHEUDRE--EPSTEIN <lucien.leheudre--epstein@etu.cyu.fr>
2025-12-02 15:42:12 +0100

    Add data structures for AVL tree and linked list

--- biblio.h
+++ biblio.h
@@ -4,3 +4,34 @@
 #include <math.h>
 #include <time.h>
 #include <unistd.h>
+
+
+
+typedef struct Chainon{
+  AVL elmt;
+  struct Chainon* next;
+}Chainon;
+
+
+
+typedef struct File{
+  struct Chainon* head;
+  struct Chainon* tail;
+}File;
+
+
+
+typedef struct AVL{
+  int elmt;
+  int equil;
+  struct AVL* fg;
+  struct AVL* fd;
+}AVL;
+
+
+
+typedef AVL* pArbre;
+
+
+
+typedef File* pFile;
