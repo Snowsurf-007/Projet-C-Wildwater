7dba1f2dcd16b7fb72c2b0c1b53eac2d840357fb
ibratra94 <ibrahima.traore4@etu.cyu.fr>
2025-12-05 10:02:37 +0100

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -14,3 +14,12 @@ fi
 cat fuite.txt
 # awk -F';' '$NF ~ /-/' fichier.csv > temp.csv  #recupere usine 
 # cut -d';' -f2,4 temp.csv>temp2.csv #recupere donnée usine
+
+
+suite (corection)
+#!/bin/bash
+if [ $# -ne 3 ]
+then
+	awk -F';' '$NF ~ /-/' la.csv > temp.txt
+	cut -d';' -f2,4  temp.txt > temp2.txt 
+fi
