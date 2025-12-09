f883dd4fbf6e89495aaea478b4917e7351955030
ibratra94 <ibrahima.traore4@etu.cyu.fr>
2025-12-09 16:13:56 +0100

    Update histo.sh for data processing and plotting
    Refactor script to process data and generate histogram PDF.

--- histo.sh
+++ histo.sh
@@ -1,28 +1,21 @@
 #!/bin/bash
-
-echo "Compilation..."
-make
-
-echo "Exécution :"
-./programme
-
 if [ $# -ne 3 ]
 then
-
+	awk -F';' '$NF ~ /-/' $1 > temp.csv
+	cut -d';' -f2,4 temp.csv > temp2.csv
+	sed -i 's/ //g' temp2.csv
+	sed -i 's/;/ /g' temp2.csv
+	sort max_vol.dat
+	head -n 50 temp2.csv > max_vol.date
+	tail -n 50 temp2.csv > max_vol.date
+	#awk -F';' '$1 ~ /$2/' la.csv > temp.csv
 fi
 
-cat fuite.txt
-# awk -F';' '$NF ~ /-/' fichier.csv > temp.csv  #recupere usine 
-# cut -d';' -f2,4 temp.csv>temp2.csv #recupere donnée usine
-
-
-suite (corection)
-#!/bin/bash
-if [ $# -ne 3 ]
-then
-	awk -F';' '$NF ~ /-/' la.csv > temp.txt
-	cut -d';' -f2,4  temp.txt > histo.txt 
-fi
-
-gnuplot> set style data histograms
-gnuplot> plot './histo.txt' using 2:xtic(1)
+gnuplot <<EOF
+set terminal pdf
+set output "hitotest.pdf
+set style data histograms
+set style fill solid 1.0 border -1
+set boxwidth 0.8
+plot "max_vol.date" using 2:xtic(1) notitle
+EOF
