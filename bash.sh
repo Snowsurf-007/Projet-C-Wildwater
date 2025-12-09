dedf76767bfb97eb575b7391d25bad9b3808ee63
Lucien LEHEUDRE--EPSTEIN <lucien.leheudre--epstein@etu.cyu.fr>
2025-12-09 15:05:26 +0100

    Refactor time calculation and clean up bash script
    Updated time calculation syntax and removed unused code.

--- bash.sh
+++ bash.sh
@@ -8,7 +8,7 @@ if [ $# -ne 3 ]
 then 
 	echo "ERREUR: le nombre d'argument donné n'est pas le bon !"
 	fin=$(date +%s.%N)
-	time=$(echo "($fin - $debut)*1000" | bc)
+	time=$(echo "($fin - $debut)*1000" |bc)
     	echo "Le programme a mis ${time} ms à être exécuté"
     	exit 1
 fi
@@ -19,41 +19,26 @@ if [ ! -r "$DATAFILE" ]
 then
 	echo "ERREUR: fichier d'entrée introuvable ou non lisible !"
 	fin=$(date +%s.%N)
-	time=$(echo "($fin - $debut)*1000" | bc)
+	time=$(echo "($fin - $debut)*1000" |bc)
     	echo "Le programme a mis ${time} ms à être exécuté"
 	exit 2
 fi
 
 
 
-#partie appel histogramme
-bash 
+case 
+
+#partie appel fonction histogramme
+#bash histo.sh
 
 #partie appel code C
+#make
+#./leaks
 
 #verif retour code C
 
 
 
 fin=$(date +%s.%N)
-time=$(echo "($fin - $debut)*1000" | bc)
+time=$(echo "($fin - $debut)*1000" |bc)
 echo "Durée totale : ${time} ms"
-
-partie histo
-if [ $# -ne 3 ]
-then
-	awk -F';' '$NF ~ /-/' la.csv > temp.csv
-	cut -d';' -f2,4  temp.csv > temp2.csv
-	sed -i 's/ //g' temp2.csv
-	sed -i 's/;/ /g' temp2.csv
-	sort 
-fi
-
-gnuplot <<EOF
-set terminal pdf
-set output "hitotest.png"
-set style data histograms
-set style fill solid 1.0 border -1
-set boxwidth 0.8
-plot "temp2.txt" using 2:xtic(1) notitle
-EOF
