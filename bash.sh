4e6d3b0bcd066cd9d0003b059a534e6248b21411
ibratra94 <ibrahima.traore4@etu.cyu.fr>
2025-12-09 14:52:44 +0100

    Implement histogram processing and plotting
    Add histogram generation logic and gnuplot configuration

--- bash.sh
+++ bash.sh
@@ -38,3 +38,22 @@ bash
 fin=$(date +%s.%N)
 time=$(echo "($fin - $debut)*1000" | bc)
 echo "Durée totale : ${time} ms"
+
+partie histo
+if [ $# -ne 3 ]
+then
+	awk -F';' '$NF ~ /-/' la.csv > temp.csv
+	cut -d';' -f2,4  temp.csv > temp2.csv
+	sed -i 's/ //g' temp2.csv
+	sed -i 's/;/ /g' temp2.csv
+	sort 
+fi
+
+gnuplot <<EOF
+set terminal pdf
+set output "hitotest.png"
+set style data histograms
+set style fill solid 1.0 border -1
+set boxwidth 0.8
+plot "temp2.txt" using 2:xtic(1) notitle
+EOF
