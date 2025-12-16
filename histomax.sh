0116ca9a07a52a7e6882756d01c6b91582594441
Lucien LEHEUDRE--EPSTEIN <lucien.leheudre--epstein@etu.cyu.fr>
2025-12-16 17:53:34 +0100

    Update histomax.sh

--- histomax.sh
+++ histomax.sh
@@ -1,43 +1,55 @@
 #!/bin/bash
 if [ $# -ne 3 ]
 then
-	awk -F';' '$NF ~ /-/' $1 > temp.csv #mettre le nom du fichier en premier arfument
+	awk -F';' '$NF ~ /-/' $1 > temp.csv #mettre le nom du fichier en premier argument
 	cut -d';' -f2,4 temp.csv > temp2.csv
 	sed -i 's/ //g' temp2.csv
 	sed -i 's/;/ /g' temp2.csv
 	sort -k2 -n temp2.csv > temp3.csv
 	head -n 50 temp3.csv > max_vol50.date
 	tail -n 10 temp3.csv > max_vol10.date
-	#transform les chiffres de la deuxiemes colones en flottant et divise
-	awk '$2/1000 ' temp3.csv > temp3div.csv
+	#transforme les chiffres de la deuxiemes colones en flottant et divise
+	awk '{ $2 = $2 / 1000; print $1, $2 }' temp3.csv > temp3div.csv
 	tail -n 10 temp3div.csv > max_vol10.date
 	head -n 50 temp3div.csv > max_vol50.date
 fi
 
 gnuplot <<EOF
-set terminal png font "Arial,3"
+set title "50 plus petites usines" font ",20" center
+set terminal png size 1600,800 font "Arial,10"
+set lmargin 10
+set rmargin 5
+set tmargin 5
+set bmargin 15
+set xtics rotate by -90 offset 0,-2 nomirror
 set terminal png
-set output "histopetits.png"
-set style data histograms
-set yrange [0:*]   # 0 jusqu’au max automatique
+set output "histolow.png"
+set xlabel "Nom des usines"
+set ylabel "Volume (M.m^3)"
+set xtics rotate by -90 font ",10" nomirror
+set yrange [0:1.25]
 set style fill solid 1.0 border -1
-set boxwidth 0.8
- #font réduit la taille de la police à 6
-set xtics rotate by -90 font ",5" nomirror
-set ylabel " en 10^-3 M.m^3 (eau traiter par an)"
-set xlabel " Nom des usines"
-plot "max_vol50.date" using 2:xtic(1) title "50 plus petites usines"
+set style data histograms
+set boxwidth 1
+plot "max_vol50.date" using 2:xtic(1) title ""
 EOF
 
 gnuplot <<EOF
-set xtics rotate by -90 font ",6" nomirror
-set terminal png font "Arial,3"
+set title "10 plus grandes usines" font ",20" center
+set terminal png size 1600,800 font "Arial,10"
+set lmargin 10
+set rmargin 5
+set tmargin 5
+set bmargin 15
+set xtics rotate by -90 offset 0,-2 nomirror
 set terminal png
-set xlabel " Nom des usines"
-set output "histogrands.png"
-set style data histograms
+set output "histohigh.png"
+set xlabel "Nom des usines"
+set ylabel "Volume (M.m^3)"
+set xtics rotate by -90 font ",10" nomirror
+set yrange [0:75000]
 set style fill solid 1.0 border -1
-set boxwidth 0.8
-set ylabel " en 10^-3 M.m^3 (eau traiter par an)"
-plot "max_vol10.date" using 2:xtic(1) title "10 plus grandes usines"
+set style data histograms
+set boxwidth 1
+plot "max_vol10.date" using 2:xtic(1) title ""
 EOF
