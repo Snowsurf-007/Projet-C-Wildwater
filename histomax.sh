308774e1d937c9f7516988721a8de5eacaa997b6
ibratra94 <ibrahima.traore4@etu.cyu.fr>
2025-12-16 15:33:12 +0100

    Update histomax.sh

--- histomax.sh
+++ histomax.sh
@@ -5,9 +5,9 @@ then
 	cut -d';' -f2,4 temp.csv > temp2.csv
 	sed -i 's/ //g' temp2.csv
 	sed -i 's/;/ /g' temp2.csv
-	sort temp2.csv
-	head -n 10 temp2.csv > max_vol10.date
-	tail -n 50 temp2.csv > max_vol50.date
+	sort temp2.csv > temp3.csv
+	head -n 10 temp3.csv > max_vol10.date
+	tail -n 50 temp3.csv > max_vol50.date
 
 fi
 
@@ -28,15 +28,3 @@ set style fill solid 1.0 border -1
 set boxwidth 0.8
 plot "max_vol50.date" using 2:xtic(1) title "50 derniers"
 EOF
-
-
-#cat temp.txt
-#echo "deuxieme"
-#cat ID.txt #fichier avec les ID
-#cat eau.txt	#fichier avec l'eau
-
-
-#cat temp.txt
-#echo "deuxieme"
-#cat ID.txt #fichier avec les ID
-#cat eau.txt	#fichier avec l'eau
