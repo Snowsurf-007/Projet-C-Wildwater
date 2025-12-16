b9e369444351d8d66bbce1d580ec69df4c7fe273
Lucien LEHEUDRE--EPSTEIN <lucien.leheudre--epstein@etu.cyu.fr>
2025-12-16 14:26:39 +0100

    Add AVL tree function declarations to biblio.h
    Added function declarations for AVL tree operations and main function.

--- biblio.h
+++ biblio.h
@@ -11,8 +11,6 @@
 #include <unistd.h>
 #define TAILLEID 20
 
-
-
 typedef struct AVL{
   int elmt;
   int equil;
@@ -34,6 +32,27 @@ typedef struct File{
   struct Chainon* head;
   struct Chainon* tail;
 }File;
+
+
+
+int main(int argc, char* argv[]);
+int max(int a, int b);
+int min(int a, int b);
+int hauteur(AVL* a);
+int equilibre(AVL* a);
+int recherche(AVL* a, char* e);
+AVL* creationAVL(int e, char* c);
+AVL* rotationGauche(AVL* a);
+AVL* rotationDroite(AVL* a);
+AVL* doubleRotationGauche(AVL* a);
+AVL* doubleRotationDroite(AVL* a);
+AVL* equilibrage(AVL* a);
+AVL* insertStrAVL(AVL* a, char* e, int* h, int nb);
+void traiter(AVL* a);
+void infixe(AVL* a);
+void prefixe(AVL* a);
+void suffixe(AVL* a);
+void recupAVL(FILE* f);
 
 
 
