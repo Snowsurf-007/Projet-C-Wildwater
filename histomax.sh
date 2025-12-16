d0b5e893fb5d201d18ec8ed34daa222dd115c691
ibratra94 <ibrahima.traore4@etu.cyu.fr>
2025-12-16 14:02:06 +0100

    Modify histomax.sh for improved data processing

--- histomax.sh
+++ histomax.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 if [ $# -ne 3 ]
 then
-	awk -F';' '$NF ~ /-/' $1 > temp.csv
+	awk -F';' '$NF ~ /-/' $1 > temp.csv #mettre le nom du fichier en premier arfument
 	cut -d';' -f2,4 temp.csv > temp2.csv
 	sed -i 's/ //g' temp2.csv
 	sed -i 's/;/ /g' temp2.csv
 	sort max_vol.dat
-	head -n 10 temp2.csv > max_vol.date
+	head -n 50 temp2.csv > max_vol.date
 	tail -n 50 temp2.csv > max_vol.date
 	#awk -F';' '$1 ~ /'$2'/' $1 > usine.csv
 fi
 
 gnuplot <<EOF
-set terminal pdf
-set output "hitotest.pdf"
+set terminal png
+set output "hitotest.png"
 set style data histograms
 set style fill solid 1.0 border -1
 set boxwidth 0.8
@@ -24,4 +24,5 @@ EOF
 #echo "deuxieme"
 #cat ID.txt #fichier avec les ID
 #cat eau.txt	#fichier avec l'eau
+
 
