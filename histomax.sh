8c17829dfd4b29a18f9e6dda56b6efa72dbf525e
Lucien LEHEUDRE--EPSTEIN <lucien.leheudre--epstein@etu.cyu.fr>
2025-12-19 15:32:16 +0100

    Update histomax.sh

--- histomax.sh
+++ histomax.sh
@@ -21,16 +21,16 @@ then
 
 gnuplot <<EOF
 set title "Histogramme des capacités maximales des 50 plus petites usines" font ",20" center
-set terminal png size 1600,800 font "Arial,10"
-set lmargin 10
+set terminal png size 1600,1000 font "Arial,12"
+set lmargin 13
 set rmargin 5
 set tmargin 5
-set bmargin 15
+set bmargin 20
 set xtics rotate by -90 offset 0,-2 nomirror
 set terminal png
-set output "histolow.png"
-set xlabel "Nom des usines"
-set ylabel "Volume (M.m^3)"
+set output "histo_max_petit.png"
+set xlabel "Nom des usines" font ",16"
+set ylabel "Volume (M.m^3)" font ",16"
 set xtics rotate by -90 font ",10" nomirror
 set yrange [0.5:1.5]
 set style fill solid 1.0 border -1
@@ -41,16 +41,16 @@ EOF
 
 gnuplot <<EOF
 set title "Histogramme des capacités maximales des 10 plus grandes usines" font ",20" center
-set terminal png size 1600,800 font "Arial,10"
-set lmargin 10
+set terminal png size 1600,1000 font "Arial,12"
+set lmargin 13
 set rmargin 5
 set tmargin 5
-set bmargin 15
+set bmargin 20
 set xtics rotate by -90 offset 0,-2 nomirror
 set terminal png
-set output "histohigh.png"
-set xlabel "Nom des usines"
-set ylabel "Volume (M.m^3)"
+set output "histo_max_grand.png"
+set xlabel "Nom des usines" font ",16"
+set ylabel "Volume (M.m^3)" font ",16"
 set xtics rotate by -90 font ",10" nomirror
 set yrange [0:75000]
 set style fill solid 1.0 border -1
