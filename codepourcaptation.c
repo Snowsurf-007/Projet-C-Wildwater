1dcfbd265f134cf28403890e13a71b04c23d1091
ibratra94 <ibrahima.traore4@etu.cyu.fr>
2025-12-19 17:07:50 +0100

    Update codepourcaptation.c

--- codepourcaptation.c
+++ codepourcaptation.c
@@ -1,265 +1,62 @@
-//#ifndef BIBLIO_H
-//#define BIBLIO_H
-/*gcc calc2.c -o affiche2
-	./affiche2 tempcapt.csv */
+#!/bin/bash
+
+if [ $# -eq 3 ]
+then
+	exit
+fi
+
+if [ $# -ne 3 ]
+then
+
+awk -F';' '$1 ~ /-/ && $2 != "-" && $3 != "-" && $4 != "-" &&  $NF != "-"' $1 > temp.csv
+cut -d';' -f3,4 temp.csv > temp1.csv
+touch temp2.csv
+gcc -o temp calc2.c 
+./temp temp1.csv temp2.csv
+grep -v '^$' temp2.csv > temp3.csv
+sort -k2 -n temp3.csv > temp4.csv
+head -n 50 temp4.csv > src_vol50.csv
+tail -n 10 temp4.csv > src_vol10.csv
+fi
+
+gnuplot <<EOF
+set title "Histogramme des volumes preleves des 50 plus petites usines" font ",20" center
+set terminal png size 1600,1000 font "Arial,12"
+set lmargin 13
+set rmargin 5
+set tmargin 5
+set bmargin 20
+set xtics rotate by -90 offset 0,-2 nomirror
+set terminal png
+set output "histo_src.png"
+set xlabel "Nom des usines" font ",16"
+set ylabel "Volume (M.m^3)" font ",16"
+set xtics rotate by -90 font ",10" nomirror
+set yrange [0.5:1.5]
+set style fill solid 1.0 border -1
+set style data histograms
+set boxwidth 1
+plot "src_vol50.csv" using 2:xtic(1) title "" lc rgb "blue"
+EOF
+
+gnuplot <<EOF
+set title "Histogramme des volumes preleves des 10 plus grandes usines" font ",20" center
+set terminal png size 1600,1000 font "Arial,12"
+set lmargin 13
+set rmargin 5
+set tmargin 5
+set bmargin 20
+set xtics rotate by -90 offset 0,-2 nomirror
+set terminal png
+set output "histo_max_grand.png"
+set xlabel "Nom des usines" font ",16"
+set ylabel "Volume (M.m^3)" font ",16"
+set xtics rotate by -90 font ",10" nomirror
+set yrange [0:75000]
+set style fill solid 1.0 border -1
+set style data histograms
+set boxwidth 1
+plot "src_vol10.csv" using 2:xtic(1) title "" lc rgb "blue"
+EOF
 
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <math.h>
-#include <time.h>
-#include <unistd.h>
-#define TAILLEID 20
-
-typedef struct AVL{
-  int elmt;
-  int equil;
-  char* ID;
-  struct AVL* fg;
-  struct AVL* fd;
-}AVL;
-
-
-
-typedef struct Chainon{
-  AVL elmt;
-  struct Chainon* next;
-}Chainon;
-
-
-typedef struct File{
-  struct Chainon* head;
-  struct Chainon* tail;
-}File;
-
-
-
-int main(int argc, char* argv[]);
-int max(int a, int b);
-int min(int a, int b);
-int hauteur(AVL* a);
-int equilibre(AVL* a);
-int recherche(AVL* a, char* e);
-AVL* creationAVL(int e, char* c);
-AVL* rotationGauche(AVL* a);
-AVL* rotationDroite(AVL* a);
-AVL* doubleRotationGauche(AVL* a);
-AVL* doubleRotationDroite(AVL* a);
-AVL* equilibrage(AVL* a);
-AVL* insertStrAVL(AVL* a, char* e, int* h, int nb);
-void traiter(AVL* a);
-void infixe(AVL* a);
-void prefixe(AVL* a);
-void suffixe(AVL* a);
-void recupAVL(FILE* f);
-
-
-
-//#endif // BIBLIO_H
-int max(int a, int b){
-    return (a > b) ? a : b;
-}
-
-int min(int a, int b){
-    return (a < b) ? a : b;
-}
-
-int hauteur(AVL* a){
-    if(a == NULL) return -1;
-    return 1 + max(hauteur(a->fg), hauteur(a->fd));
-}
-
-int equilibre(AVL* a){
-    if(a == NULL) return 0;
-    return hauteur(a->fd) - hauteur(a->fg);
-}
-
-int recherche(AVL* a, char* e){
-    if(a == NULL)
-        return 1;
-
-    int z = strcmp(a->ID, e);
-
-    if(z == 0)
-        return 0;
-    else if(z > 0)
-        return recherche(a->fg, e);
-    else
-        return recherche(a->fd, e);
-}
-void traiter(AVL* a){
-    if(a != NULL)
-        printf("%d (%s)  eq=%d\n", a->elmt, a->ID, a->equil);
-}
-
-void infixe(AVL* a){
-    if(a == NULL) return;
-    infixe(a->fg);
-    traiter(a);
-    infixe(a->fd);
-}
-
-void prefixe(AVL* a){
-    if(a == NULL) return;
-    traiter(a);
-    prefixe(a->fg);
-    prefixe(a->fd);
-}
-
-void suffixe(AVL* a){
-    if(a == NULL) return;
-    suffixe(a->fg);
-    suffixe(a->fd);
-    traiter(a);
-}
-AVL* creationAVL(int e, char* c){
-    AVL* newNode = malloc(sizeof(AVL));
-    if(!newNode){
-        printf("Erreur : malloc\n");
-        exit(1);
-    }
-
-    newNode->elmt = e;
-    newNode->fg = NULL;
-    newNode->fd = NULL;
-    newNode->equil = 0;
-    newNode->ID = strdup(c);
-
-    return newNode;
-}
-
-/* =====================
-   ROTATION GAUCHE
-   ===================== */
-AVL* rotationGauche(AVL* a){
-    AVL* p = a->fd;
-
-    a->fd = p->fg;
-    p->fg = a;
-
-    a->equil = equilibre(a);
-    p->equil = equilibre(p);
-
-    return p;
-}
-
-/* =====================
-   ROTATION DROITE
-   ===================== */
-AVL* rotationDroite(AVL* a){
-    AVL* p = a->fg;
-
-    a->fg = p->fd;
-    p->fd = a;
-
-    a->equil = equilibre(a);
-    p->equil = equilibre(p);
-
-    return p;
-}
-
-AVL* doubleRotationGauche(AVL* a){
-    a->fd = rotationDroite(a->fd);
-    return rotationGauche(a);
-}
-
-AVL* doubleRotationDroite(AVL* a){
-    a->fg = rotationGauche(a->fg);
-    return rotationDroite(a);
-}
-
-/* =====================
-   ÉQUILIBRAGE
-   ===================== */
-AVL* equilibrage(AVL* a){
-    a->equil = equilibre(a);
-
-    if(a->equil == 2){ // trop à droite
-        if(a->fd->equil < 0)
-            return doubleRotationGauche(a);
-        else
-            return rotationGauche(a);
-    }
-    else if(a->equil == -2){ // trop à gauche
-        if(a->fg->equil > 0)
-            return doubleRotationDroite(a);
-        else
-            return rotationDroite(a);
-    }
-
-    return a;
-}
-
-/* =====================
-   INSERTION AVL
-   ===================== */
-AVL* insertStrAVL(AVL* a, char* e,int* h,int capter) {
-    if (a == NULL) {
-    	*h=1;
-        return creationAVL(capter, e);
-    }
-
-    int z = strcmp(a->ID, e);
-
-    if (z == 0) {
-        //printf("Cet élément est déjà présent dans l'AVL\n");
-        a->elmt = a->elmt+ capter; 
-        return a;
-    }
-    else if (z > 0) {
-        a->fg = insertStrAVL(a->fg, e, h, capter);
-        *h=-*h;
-    }
-    else if(z < 0){
-        a->fd = insertStrAVL(a->fd, e, h, capter);
-    }
-    else{
-        *h=0;
-        return a;
-    }
-    if(*h!=0){
-        a->equil=a->equil+*h;
-        a=equilibrage(a);
-        if(a->equil==0){
-            *h=0;
-        }
-        else{
-            *h=1;
-        }
-    }
-    return a;
-}
-void ecrire(FILE * f, AVL* usine ) {
-
-    if(usine!=NULL){
-        ecrire(f,usine->fg);
-        fprintf(f, "%s;%d",usine->ID, usine->elmt);
-        ecrire(f, usine->fd);
-    }
-}
-int main(int argc, char* argv[]){
-	int h;
-	h=0;
-    FILE* f=NULL;
-    FILE* fichier=NULL;
-    f=fopen(argv[1],"r+");
-    if(f==NULL){
-        exit(45);
-    }
-    fichier = fopen(argv[2], "w+");
-    if(fichier==NULL){
-        exit(0);
-    }
-    int a;
-    char* ID =malloc(TAILLEID*sizeof(char));
-    AVL* usine= NULL; 
-    while (fscanf(f, "%1000[^;]; %d", ID, &a) == 2) {
-    	usine = insertStrAVL(usine, ID, &h, a);
-    }                                    
-    ecrire(fichier, usine);
-    fputs("dsffdf", fichier);
-    fclose(fichier);
-    fclose(f);
-    return 0;
-}
