27d1bf2ef7c7f6a911dab5e7f0afd449ea98bbb6
Lucien LEHEUDRE--EPSTEIN <lucien.leheudre--epstein@etu.cyu.fr>
2025-12-20 23:57:31 +0100

    Update histosrc.sh

--- histosrc.sh
+++ histosrc.sh
@@ -36,7 +36,7 @@ set yrange [0.5:1.5]
 set style fill solid 1.0 border -1
 set style data histograms
 set boxwidth 1
-plot "src_vol50.csv" using 2:xtic(1) title "" lc rgb "blue"
+plot "src_vol50.csv" using 2:xtic(1) title "" lc rgb "magenta"
 EOF
 
 gnuplot <<EOF
@@ -56,7 +56,7 @@ set yrange [0:75000]
 set style fill solid 1.0 border -1
 set style data histograms
 set boxwidth 1
-plot "src_vol10.csv" using 2:xtic(1) title "" lc rgb "blue"
+plot "src_vol10.csv" using 2:xtic(1) title "" lc rgb "magenta"
 EOF
 
 
