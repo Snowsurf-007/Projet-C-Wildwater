8fe62cd9482bebc9ed8d44411e0c951c6a45efe8
Bibi110 <hugo.treny@etu.cyu.fr>
2025-12-20 14:54:52 +0100

    Update biblio.h

--- biblio.h
+++ biblio.h
@@ -11,20 +11,22 @@
 #include <unistd.h>
 #define TAILLEID 20
 
-typedef struct AVL{
-  int elmt;
-  int equil;
-  Char* ID
-  struct AVL* fg;
-  struct AVL* fd;
-}AVL;
+typedef struct AVL {
+	struct Arbre* arbre;
+	int equil;
+	char* ID;
+	struct AVL* fg;
+	struct AVL* fd;
+} AVL;
 
 
 
-typedef struct Chainon{
-  AVL elmt;
-  struct Chainon* next;
-}Chainon;
+typedef struct Arbre {
+	struct Chainon* enfants;
+	float elmt;
+	float litre;
+	char*ID;
+} Arbre;
 
 
 
@@ -33,27 +35,43 @@ typedef struct File{
   struct Chainon* tail;
 }File;
 
-
+typedef struct Chainon {
+	struct Arbre* enfant;
+	struct Chainon* next;
+} Chainon;
 
 int main(int argc, char* argv[]);
 int max(int a, int b);
 int min(int a, int b);
 int hauteur(AVL* a);
 int equilibre(AVL* a);
-int recherche(AVL* a, char* e);
-AVL* creationAVL(int e, char* c);
+
+/* ======== Fonctions AVL ======== */
+void recherche(AVL* a, char* e,Arbre* bebe);
+AVL* creationAVL(Arbre* e, char* c);
+
 AVL* rotationGauche(AVL* a);
 AVL* rotationDroite(AVL* a);
 AVL* doubleRotationGauche(AVL* a);
 AVL* doubleRotationDroite(AVL* a);
+
 AVL* equilibrage(AVL* a);
-AVL* insertStrAVL(AVL* a, char* e, int* h, int nb);
+
+AVL* insertStrAVL(AVL* a, char* e, int* h, Arbre*p);
+
+/* ======== Affichage ======== */
 void traiter(AVL* a);
 void infixe(AVL* a);
 void prefixe(AVL* a);
 void suffixe(AVL* a);
-void recupAVL(FILE* f);
 
+void test(FILE* f);
 
+Chainon* creationChainon(Arbre* bebe);
+Chainon* empiler(Chainon* pliste, Arbre* bebe);
+Arbre* creation(char* idav);
+Arbre* creation2(char* idam, char* idav, float fuite);
+void affichage(Arbre* a,int niveau);
+Arbre* mega_arbre(FILE* US, FILE* SJ, FILE* JR, FILE* RU);
 
 #endif // BIBLIO_H
