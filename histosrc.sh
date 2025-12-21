43f367d42f0b8119647978c97a20785ca0794e4f
ibratra94 <ibrahima.traore4@etu.cyu.fr>
2025-12-21 22:14:54 +0100

    Update histosrc.sh for argument handling and processing
    Refactor script to handle three arguments and update processing steps.

--- histosrc.sh
+++ histosrc.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
-
-if [ $# -ne 1 ]
+set -x
+if [ $# -eq 3 ]
 then
-	exit 20
+exit
 fi
 
-if [ $# -eq 1 ]
+if [ $# -ne 3 ]
 then
-
-	awk -F';' '$1 ~ /-/ && $2 != "-" && $3 != "-" && $4 != "-" &&  $NF != "-"' $1 > temp.csv
-	cut -d';' -f3,4 temp.csv > temp1.csv
-	touch temp2.csv
-	make leaks
-	./leaks temp1.csv temp2.csv
-	grep -v '^$' temp2.csv > temp3.csv
-	sort -k2 -n temp3.csv > temp4.csv
-	head -n 50 temp4.csv > src_vol50.csv
-	tail -n 10 temp4.csv > src_vol10.csv
+awk -F';' '$1 ~ /-/ && $2 != "-" && $3 != "-" && $4 != "-" &&  $NF != "-"' c-wildwater_v3.dat > temp.csv
+head temp.csv
+cut -d';' -f3,4 temp.csv > temp1.csv
+touch temp2.csv
+gcc -o temp main.c
+./temp temp1.csv temp2.csv
+grep -v '^$' temp2.csv > temp3.csv
+sed  's/ //g' temp3.csv > temp4.csv
+sort -t';' -k2 -n temp4.csv > temp5.csv
+sed  's/;/ /g' temp5.csv > temp6.csv
+head -n 50 temp6.csv > src_vol50.csv
+tail -n 10 temp6.csv > src_vol10.csv
+cat src_vol10.csv
+cat src_vol50.csv
 fi
 
 gnuplot <<EOF
@@ -28,15 +32,15 @@ set tmargin 5
 set bmargin 20
 set xtics rotate by -90 offset 0,-2 nomirror
 set terminal png
-set output "histo_src_petit.png"
-set xlabel "Nom des usines" font ",16" offset 0, -5
+set output "histo_src.png"
+set xlabel "Nom des usines" font ",16"
 set ylabel "Volume (M.m^3)" font ",16"
 set xtics rotate by -90 font ",10" nomirror
 set yrange [0.5:1.5]
 set style fill solid 1.0 border -1
 set style data histograms
 set boxwidth 1
-plot "src_vol50.csv" using 2:xtic(1) title "" lc rgb "cyan"
+plot "src_vol50.csv" using 2:xtic(1) title "" lc rgb "blue"
 EOF
 
 gnuplot <<EOF
@@ -56,13 +60,6 @@ set yrange [0:75000]
 set style fill solid 1.0 border -1
 set style data histograms
 set boxwidth 1
-plot "src_vol10.csv" using 2:xtic(1) title "" lc rgb "cyan"
+plot "src_vol10.csv" using 2:xtic(1) title "" lc rgb "blue"
 EOF
 
-	sort -k1 -r temp2.csv > vol_src.csv
-
-	#rm temp*.csv
-	rm src_vol10.csv
-	rm src_vol50.csv
-
-fi
