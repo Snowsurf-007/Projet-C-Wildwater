ca2f89e451c182dd3a5f56273e57aafdda0fb588
Lucien LEHEUDRE--EPSTEIN <lucien.leheudre--epstein@etu.cyu.fr>
2025-12-21 23:33:40 +0100

    Update histosrc.sh

--- histosrc.sh
+++ histosrc.sh
@@ -1,70 +1,71 @@
 #!/bin/bash
-set -x
+
+fichier="$1"
+
 if [ $# -eq 3 ]
 then
-exit
+	exit
 fi
 
 if [ $# -ne 3 ]
 then
-awk -F';' '$1 ~ /-/ && $2 != "-" && $3 != "-" && $4 != "-" &&  $NF != "-"' c-wildwater_v3.dat > temp.csv
-head temp.csv
-cut -d';' -f3,4 temp.csv > temp1.csv
-touch temp2.csv
-gcc -o temp main.c
-./temp temp1.csv temp2.csv
-grep -v '^$' temp2.csv > temp3.csv
-sed  's/ //g' temp3.csv > temp4.csv
-sort -t';' -k2 -n temp4.csv > temp5.csv
-sed  's/;/ /g' temp5.csv > temp6.csv
-head -n 50 temp6.csv > src_vol50.csv
-tail -n 10 temp6.csv > src_vol10.csv
-cat src_vol10.csv
-cat src_vol50.csv
-fi
+	awk -F';' '$1 ~ /-/ && $2 != "-" && $3 != "-" && $4 != "-" &&  $NF != "-"' "$fichier" > temp.csv
+	head temp.csv
+	cut -d';' -f3,4 temp.csv > temp1.csv
+	touch temp2.csv
+	gcc -o captation captation.c
+	./captation temp1.csv temp2.csv
+	grep -v '^$' temp2.csv > temp3.csv
+	sed  's/ //g' temp3.csv > temp4.csv
+	sort -t';' -k2 -n temp4.csv > temp5.csv
+	sed  's/;/ /g' temp5.csv > temp6.csv
+	head -n 50 temp6.csv > src_vol50.csv
+	tail -n 10 temp6.csv > src_vol10.csv
 
 gnuplot <<EOF
-set terminal png
-set title "Histogramme des volumes preleves des 50 plus petites usines" font ",20"
+set title "Histogramme des capacités maximales des 50 plus petites usines" font ",20" center
 set terminal png size 1600,1000 font "Arial,12"
-set output "histopetites_src.png"
-set lmargin 15
-set bmargin 25
-set xlabel "Nom des usines" font ",16"
-set ylabel "Volume (m³)" font ",16"
-set style data histograms
-set style fill solid 1.0 border -1
-set boxwidth 0.8
-set xtics rotate by -90 font ",8" offset 0,-4 nomirror
-set ytics nomirror
-set yrange [0:*]
-set format y "%.0s%c"
-plot "src_vol50.csv" using 2:xtic(1) notitle lc rgb "blue"
-
-EOF
-
-gnuplot <<EOF
-gnuplot <<EOF
-set terminal png size 1600,1000 font "Arial,12"
-set output "histo_max_grand.png"
-set title "Histogramme des volumes preleves des 10 plus grandes usines" font ",20"
-set lmargin 15
-set bmargin 22
+set lmargin 13
 set rmargin 5
 set tmargin 5
-set xlabel "Nom des usines" font ",16"
-set ylabel "Volume (m³)" font ",16"
-set style data histograms
+set bmargin 20
+set xtics rotate by -90 offset 0,-2 nomirror
+set terminal png
+set output "histo_src_petit.png"
+set xlabel "Nom des usines" font ",16" offset 0, -5
+set ylabel "Volume (M.m^3)" font ",16"
+set xtics rotate by -90 font ",10" nomirror
+set yrange [0.5:*]
 set style fill solid 1.0 border -1
-set boxwidth 0.8
-set xtics rotate by -90 font ",10" offset 0,-3 nomirror
-set ytics nomirror
+set style data histograms
+set boxwidth 1
+plot "src_vol50.csv" using 2:xtic(1) notitle lc rgb "magenta"
+EOF
+
+gnuplot <<EOF
+set title "Histogramme des capacités maximales des 10 plus grandes usines" font ",20" center
+set terminal png size 1600,1000 font "Arial,12"
+set lmargin 13
+set rmargin 5
+set tmargin 5
+set bmargin 20
+set xtics rotate by -90 offset 0,-2 nomirror
+set terminal png
+set output "histo_src_grand.png"
+set xlabel "Nom des usines" font ",16"
+set ylabel "Volume (M.m^3)" font ",16"
+set xtics rotate by -90 font ",10" nomirror
 set yrange [0:*]
-set format y "%.0s%c"
-
-plot "src_vol10.csv" using 2:xtic(1) notitle lc rgb "red"
+set style fill solid 1.0 border -1
+set style data histograms
+set boxwidth 1
+plot "src_vol10.csv" using 2:xtic(1) title "" lc rgb "magenta"
 EOF
 
+sort -k1 -r temp2.csv > vol_src.csv
 
-EOF
+	rm temp*.csv
+	rm src_vol10.csv
+	rm src_vol50.csv
 
+fi
