51df2ba65c28e2eae63def2cae772dd9d300658d
Lucien LEHEUDRE--EPSTEIN <lucien.leheudre--epstein@etu.cyu.fr>
2025-12-21 01:50:01 +0100

    Update histoall.sh

--- histoall.sh
+++ histoall.sh
@@ -2,12 +2,35 @@
 
 if [ $# -ne 1 ]
 then
-	exit 1
+	exit 40
 fi
 
 if [ $# -eq 1 ]
 then
-
+	fichier="$1"
+	
+	bash histomax.sh "$fichier"
+	if [ $ret -ne 0 ] 
+	then
+		exit 41
+	fi
+	bash histosrc.sh "$fichier"
+	if [ $ret -ne 0 ] 
+	then
+		exit 42
+	fi
+	bash historeal.sh "$fichier"
+	if [ $ret -ne 0 ] 
+	then
+		exit 43
+	fi
+	
+	cut -d';' -f2,4 temp.csv > temp2.csv
+	
+	tail -n 10 vol_all.csv > all_vol10.csv
+	head -n 50 vol_all.csv > all_vol50.csv
+	
+fi
 gnuplot <<EOF
 set title "Histogramme global des 50 plus petites usines" font ",20" center
 set terminal png size 1600,1000 font "Arial,12"
@@ -17,7 +40,7 @@ set tmargin 5
 set bmargin 20
 set xtics rotate by -90 offset 0,-2 nomirror
 set terminal png
-set output "histo_max_petit.png"
+set output "histo_all_petit.png"
 set xlabel "Nom des usines" font ",16" offset 0, -5
 set ylabel "Volume (M.m^3)" font ",16"
 set xtics rotate by -90 font ",10" nomirror
@@ -37,7 +60,7 @@ set tmargin 5
 set bmargin 20
 set xtics rotate by -90 offset 0,-2 nomirror
 set terminal png
-set output "histo_max_grand.png"
+set output "histo_all_grand.png"
 set xlabel "Nom des usines" font ",16"
 set ylabel "Volume (M.m^3)" font ",16"
 set xtics rotate by -90 font ",10" nomirror
@@ -47,5 +70,3 @@ set style data histograms
 set boxwidth 1
 plot "max_vol10.csv" using 2:xtic(1), "" lc rgb "cyan" using3:xtic(1), "" lc rgb "magenta" using4:xtic(1), "" lc rgb "yellow" title
 EOF
-
-fi
