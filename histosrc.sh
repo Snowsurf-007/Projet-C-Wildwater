8127682d93676e8936458cc445446f841fc51ebb
Lucien LEHEUDRE--EPSTEIN <lucien.leheudre--epstein@etu.cyu.fr>
2025-12-22 13:53:01 +0100

    Update histosrc.sh

--- histosrc.sh
+++ histosrc.sh
@@ -12,8 +12,8 @@ then
 	awk -F';' '$1 ~ /-/ && $2 != "-" && $3 != "-" && $4 != "-" &&  $NF != "-"' "$fichier" > temp.csv
 	cut -d';' -f3,4 temp.csv > temp1.csv
 	touch temp2.csv
-	gcc -o captation captation.c
-	./captation temp1.csv temp2.csv
+	gcc -o histosrc histosrc.c
+	./histosrc temp1.csv temp2.csv
 	grep -v '^$' temp2.csv > temp3.csv
 	sed  's/ //g' temp3.csv > temp4.csv
 	sort -t';' -k2 -n temp4.csv > temp5.csv
