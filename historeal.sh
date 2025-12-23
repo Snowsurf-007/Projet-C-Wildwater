a4ed44852044246549f073c9ea17d0fb193494fe
Lucien LEHEUDRE--EPSTEIN <lucien.leheudre--epstein@etu.cyu.fr>
2025-12-23 19:09:12 +0100

    Update historeal.sh

--- historeal.sh
+++ historeal.sh
@@ -5,10 +5,9 @@ then
 	exit
 fi
 
-fichier="$1"
-
 if [ $# -eq 1 ]
 then
+	fichier="$1"
 	awk -F';' '$1 ~ /-/ && $2 != "-" && $3 != "-" && $4 != "-" &&  $NF != "-"' "$fichier" > temp.csv
 	cut -d';' -f3,4,5 temp.csv > temp1.csv
 	sed  's/ //g' temp1.csv > temp2.csv
