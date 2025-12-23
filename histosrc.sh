de61ac8951b7686d0dee1e2476a23d2af065e91e
Lucien LEHEUDRE--EPSTEIN <lucien.leheudre--epstein@etu.cyu.fr>
2025-12-23 19:17:07 +0100

    Update histosrc.sh

--- histosrc.sh
+++ histosrc.sh
@@ -30,7 +30,6 @@ set rmargin 5
 set tmargin 5
 set bmargin 20
 set xtics rotate by -90 offset 0,-2 nomirror
-set terminal png
 set output "histo_src_petit.png"
 set xlabel "Nom des usines" font ",16" offset 0, -5
 set ylabel "Volume (M.m^3)" font ",16"
@@ -50,7 +49,6 @@ set rmargin 5
 set tmargin 5
 set bmargin 20
 set xtics rotate by -90 offset 0,-2 nomirror
-set terminal png
 set output "histo_src_grand.png"
 set xlabel "Nom des usines" font ",16"
 set ylabel "Volume (M.m^3)" font ",16"
