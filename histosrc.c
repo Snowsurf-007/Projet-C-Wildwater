b94b2627425cb1cdcb8e6b0fdd7ecdef2ec01d34
Lucien LEHEUDRE--EPSTEIN <lucien.leheudre--epstein@etu.cyu.fr>
2025-12-23 18:07:28 +0100

    Update histosrc.c

--- histosrc.c
+++ histosrc.c
@@ -2,50 +2,6 @@
 
 
 
-typedef struct AVL{
-  int elmt;
-  int equil;
-  char* ID;
-  struct AVL* fg;
-  struct AVL* fd;
-}AVL;
-
-
-
-typedef struct Chainon{
-  AVL elmt;
-  struct Chainon* next;
-}Chainon;
-
-
-
-typedef struct File{
-  struct Chainon* head;
-  struct Chainon* tail;
-}File;
-
-
-
-int max(int a, int b);
-int min(int a, int b);
-int hauteur(AVL* a);
-int equilibre(AVL* a);
-int recherche(AVL* a, char* e);
-AVL* creationAVL(int e, char* c);
-AVL* rotationGauche(AVL* a);
-AVL* rotationDroite(AVL* a);
-AVL* doubleRotationGauche(AVL* a);
-AVL* doubleRotationDroite(AVL* a);
-AVL* equilibrage(AVL* a);
-AVL* insertStrAVL(AVL* a, char* e, int* h, int nb);
-void traiter(AVL* a);
-void infixe(AVL* a);
-void prefixe(AVL* a);
-void suffixe(AVL* a);
-void recupAVL(FILE* f);
-
-
-
 //#endif // BIBLIO_H
 int max(int a, int b){
     return (a > b) ? a : b;
@@ -182,7 +138,7 @@ AVL* equilibrage(AVL* a){
 /* =====================
    INSERTION AVL
    ===================== */
-AVL* insertStrAVL(AVL* a, char* e,int* h,int capter) {
+AVL* insertAVL(AVL* a, char* e,int* h, int capter) {
     if (a == NULL) {
     	*h=1;
         return creationAVL(capter, e);
@@ -196,11 +152,11 @@ AVL* insertStrAVL(AVL* a, char* e,int* h,int capter) {
         return a;
     }
     else if (z > 0) {
-        a->fg = insertStrAVL(a->fg, e, h, capter);
+        a->fg = insertAVL(a->fg, e, h, capter);
         *h=-*h;
     }
     else if(z < 0){
-        a->fd = insertStrAVL(a->fd, e, h, capter);
+        a->fd = insertAVL(a->fd, e, h, capter);
     }
     else{
         *h=0;
@@ -225,7 +181,7 @@ void ecrire(FILE * f, AVL* usine) {
 
     if(usine!=NULL){
         ecrire(f,usine->fg);
-        fprintf(f, "%s;%d\n",usine->ID, usine->elmt);
+        fprintf(f, "%s;%f\n",usine->ID, usine->elmt);
         ecrire(f, usine->fd);
     }
 }
@@ -249,7 +205,7 @@ int main(int argc, char* argv[]){
     char* ID =malloc(TAILLEID*sizeof(char));
     AVL* usine= NULL; 
     while (fscanf(f, "%100[^;];%d", ID, &a) == 2) {
-    	usine = insertStrAVL(usine, ID, &h, a);
+    	usine = insertAVL(usine, ID, &h, a);
     }
     ecrire(fichier, usine);
     fclose(fichier);
