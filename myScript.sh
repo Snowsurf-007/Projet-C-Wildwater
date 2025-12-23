49e3acd0eef6f37343eedcfe1f4b9efac11ce732
Lucien LEHEUDRE--EPSTEIN <lucien.leheudre--epstein@etu.cyu.fr>
2025-12-23 19:03:44 +0100

    Update myScript.sh

--- myScript.sh
+++ myScript.sh
@@ -112,24 +112,18 @@ case "$commande" in
 				fi
 				;;
 			all)	
-				echo "La fonction de l'histogramme all n'est pas disponible pour l'instant ;/ !"
-				fin=$(date +%s.%N)
-				time=`echo "$fin-$debut" | bc`
-				time=`echo "$time*1000" | bc`
-	    			echo "Le programme a mis $time ms à être exécuté"
-				exit
-				#bash histoall.sh "$fichier"
+				bash histoall.sh "$fichier"
 				#verif retour code
-				#ret=$?
-				#if [ $ret -ne 0 ] 
-				#then
-					#echo "Échec de la création de l'histogramme !"
-					#fin=$(date +%s.%N)
-					#time=`echo "$fin-$debut" | bc`
-					#time=`echo "$time*1000" | bc`
-		    			#echo "Le programme a mis $time ms à être exécuté"
-					#exit 7
-				#fi
+				ret=$?
+				if [ $ret -ne 0 ] 
+				then
+					echo "Échec de la création de l'histogramme !"
+					fin=$(date +%s.%N)
+					time=`echo "$fin-$debut" | bc`
+					time=`echo "$time*1000" | bc`
+		    			echo "Le programme a mis $time ms à être exécuté"
+					exit 7
+				fi
 				;;
 			*)
 				echo "Commande inconnue, veuillez relancer le programme"
