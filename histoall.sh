7580eec692f48ac23f28bfb0a629aad1a2924c49
Lucien LEHEUDRE--EPSTEIN <lucien.leheudre--epstein@etu.cyu.fr>
2025-12-24 18:44:16 +0100

    Update histoall.sh

--- histoall.sh
+++ histoall.sh
@@ -11,7 +11,6 @@ then
 	bash histomax.sh "$fichier"
 	bash histosrc.sh "$fichier"
 	bash historeal.sh "$fichier"
-
 	touch temp4.csv
 	cd ./fichiers_resultats
 	awk 'NR > 1' vol_max.csv > temp1.csv
@@ -20,8 +19,6 @@ then
 	mv temp1.csv ../
 	mv temp2.csv ../
 	mv temp3.csv ../
-
-
 	cd ..
 	make histo
 	./histo 3 temp1.csv temp2.csv temp3.csv temp4.csv > fusion.csv
@@ -32,13 +29,18 @@ then
 	sed  's/;/ /g' temp6.csv > vol_all.csv
 	tail -n 10 vol_all.csv > all_vol10.csv
 	head -n 50 vol_all.csv > all_vol50.csv
+	echo "les histogrammes pour all ne sont pas bon, les autres si"
 
 gnuplot <<EOF
 set title "Histogramme global des 50 plus petites usines" font ",20" center
 set terminal png size 1600,1000 font "Arial,12"
 set lmargin 13
 set rmargin 5
-@@ -42,7 +43,7 @@
+set tmargin 5
+set bmargin 20
+set xtics rotate by -90 offset 0,-2 nomirror
+set terminal png
+set output "histo_all_petit.png"
 set xlabel "Nom des usines" font ",16" offset 0, -5
 set ylabel "Volume (M.m^3)" font ",16"
 set xtics rotate by -90 font ",10" nomirror
@@ -46,7 +48,9 @@ set yrange [0.0:*]
 set style fill solid 1.0 border -1
 set style data histograms
 set boxwidth 1
-@@ -52,19 +53,19 @@
+set style histogram rowstacked
+plot "all_vol50.csv" using 2:xtic(1) lc rgb "cyan"    title "Colonne 2", \
+     ""             using 3          lc rgb "magenta" title "Colonne 3", \
      ""             using 4          lc rgb "yellow"  title "Colonne 4"
 EOF
 gnuplot <<EOF
@@ -66,7 +70,8 @@ set yrange [0:*]
 set style fill solid 1.0 border -1
 set style data histograms
 set boxwidth 1
-@@ -73,3 +74,17 @@
+set style histogram rowstacked
+plot "all_vol10.csv" using 2:xtic(1) lc rgb "cyan"    title "Colonne 2", \
      ""             using 3          lc rgb "magenta" title "Colonne 3", \
      ""             using 4          lc rgb "yellow"  title "Colonne 4"
 EOF
