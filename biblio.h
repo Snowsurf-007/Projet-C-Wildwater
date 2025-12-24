0de3c958bbafd23af8b59ca11328d94e41b29fde
Lucien LEHEUDRE--EPSTEIN <lucien.leheudre--epstein@etu.cyu.fr>
2025-12-24 19:31:01 +0100

    Update biblio.h

--- biblio.h
+++ biblio.h
@@ -56,6 +56,7 @@ int min(int a, int b);
 int hauteur(AVL* a);
 int equilibre(AVL* a);
 int recherche(AVL* a, char* e);
+AVL* rechercherAVL(AVL* a, char* id);
 
 // Rotations et équilibrage
 AVL* rotationGauche(AVL* a);
@@ -72,6 +73,11 @@ AVL* insertAVL(AVL* a, char* e, int* h, int capter);
 AVL* insertRealAVL(AVL* a, char* e, int* h, float max, float real, float src);
 AVL* insertStrAVL(AVL* a, char* e, int* h, Arbre* p);
 AVL* insert_enfant(AVL* a, char* e, int* h, Arbre* enfant);
+Chainon* creationChainon(Arbre* bebe);
+Chainon* empiler(Chainon* pliste, Arbre* bebe);
+Arbre* insert(char* id, int nb);
+AVL* get_or_create(AVL* avl, char* id, int* h, Arbre** res);
+Arbre* mega_arbre(FILE* US, FILE* SJ, FILE* JR, FILE* RU);
 
 // Affichage et traitement
 void traiter(AVL* a);
@@ -80,6 +86,8 @@ void infixe(AVL* a);
 void infixeReal(AVL* a);
 void prefixe(AVL* a);
 void suffixe(AVL* a);
+void afficherABR(Arbre* a, int niveau);
+void afficherAVL(AVL* a);
 
 // Fichiers
 void ecrire(FILE* f, AVL* usine);
@@ -87,7 +95,18 @@ void ecrireReal(FILE* f, AVL* usine);
 
 // Libération mémoire
 void libererAVL(AVL* a);
+void libererArbre(Arbre* a);
 
 void calcul(Arbre* a, double* somme_fuites);
 float source(FILE* f);
+
+void mode1(char* inputFile, char* outputFile);
+void mode2(char* inputFile, char* outputFile);
+void mode3(char* maxFile, char* realFile, char* srcFile, char* outputFile);
+void mode4(char* suFile, char* usFile, char* sjFile, char* jrFile, char* ruFile);
+
+int main(int argc, char* argv[]);
+
+
+
 #endif // BIBLIO_H
